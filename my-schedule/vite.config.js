ba5ac3c2663d9c2f49066bf2ae86dba84e7c8a5d
Андрей Соколов <musorsky@outlook.com>
2026-09-04 18:54:19 +0300

    Добавил оффлайн режим PWA

--- my-schedule/vite.config.js
+++ my-schedule/vite.config.js
@@ -1,8 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/my-schedule/', // ВАЖНО: тут должно быть имя твоего репозитория в слэшах!
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'] // Кэшируем все файлы для оффлайна
+      },
+      manifest: {
+        name: 'Расписание',
+        short_name: 'Расписание',
+        description: 'Расписание занятий',
+        theme_color: '#0f0f0f',
+        background_color: '#0f0f0f',
+        display: 'standalone', // Именно эта строчка убирает адресную строку браузера (Safari)
+        icons: [
+          {
+            src: 'icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ],
+  base: '/my-schedule/', // Твой репозиторий
 })
